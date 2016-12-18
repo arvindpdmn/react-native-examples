b7da49ef2dd71281777793cd4299ce7f43a20234
Arvind Padmanabhan <arvindpdmn@gmail.com>
2016-12-18 14:07:09 +0530

    Changed component name to HelloWorldApp.

--- helloWorld/index.android.js
+++ helloWorld/index.android.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class helloWorld extends Component {
+export default class HelloWorldApp extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('helloWorld', () => helloWorld);
+AppRegistry.registerComponent('helloWorld', () => HelloWorldApp);

ae0202dfb2165a5f8a545239cc296b2b8c2d3203
Arvind Padmanabhan <arvindpdmn@gmail.com>
2016-12-19 11:21:51 +0530

    Added image and custom components to helloWorld example.

--- helloWorld/index.android.js
+++ helloWorld/index.android.js
@@ -9,11 +9,43 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true}; // not passed from parent: a changing state of this component
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      // Shows styling controlled by parent component via props
+      <Text style={this.props.style}>{display}</Text>
+    );
+  }
+}
 
 export default class HelloWorldApp extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -26,6 +58,20 @@ export default class HelloWorldApp extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+
+        {/* Shows the use of props: source and style */}
+        <Image source={pic} style={{width: 193, height: 110}}/>
+
+        {/* Shows the use of custom component Greeting */}
+        <View style={{alignItems: 'center'}}>
+          <Greeting name='Asian' />
+          <Greeting name='Indian' />
+          <Greeting name='Bengalurean' />
+        </View>
+
+        <View>
+          <Blink text='I love to blink' style={styles.salmon} />
+        </View>
       </View>
     );
   }
@@ -47,6 +93,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  salmon: {
+    color: 'salmon',
   },
 });
 

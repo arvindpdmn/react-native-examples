0807e6032b689b2f14545475e86d722a9db8e348
Arvind Padmanabhan <arvindpdmn@gmail.com>
2016-12-19 13:23:01 +0530

    Added new ways of using flexbox.

--- flexbox/index.android.js
+++ flexbox/index.android.js
@@ -6,11 +6,24 @@ class FlexDimensionsBasics extends Component {
     return (
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        <View style={{height: 100}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'stretch'}}>
+          <View style={{width: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        </View>
+
+        <View style={{flex: 2, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
+          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+          <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        </View>
       </View>
     );
   }

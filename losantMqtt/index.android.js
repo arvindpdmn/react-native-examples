ba9d2ede17c0af89f6c3b6ad9ce628d57b894130
Arvind Padmanabhan <arvindpdmn@gmail.com>
2016-12-20 14:34:02 +0530

    Added losantMqtt example. Not working. Used react-native-browser-builtins to overcome some errors: util, buffer, etc.

---
+++ losantMqtt/index.android.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var Buffer = require('buffer').Buffer;
+
+var Device = require('losant-mqtt').Device;
+
+// Construct device.
+var device = new Device({
+  id: 'my-device-id',
+  key: 'my-app-access-key',
+  secret: 'my-app-access-secret'
+});
+
+// Connect to Losant.
+device.connect();
+
+// Listen for commands.
+device.on('command', function(command) {
+  console.log('Command received.');
+  console.log(command.name);
+  console.log(command.payload);
+});
+
+// Send temperature once every second.
+setInterval(function() {
+  device.sendState({ temperature: readAnalogIn() });
+}, 1000);
+
+export default class losantMqtt extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('losantMqtt', () => losantMqtt);
